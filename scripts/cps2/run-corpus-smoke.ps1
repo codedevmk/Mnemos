b09bce7a781da39c850bb7dc0967e3cdcd659bd8
codedevmk <codedevmk@users.noreply.github.com>
2026-06-26 00:53:31 -0500

    Add Mars Matrix CPS2 audio corpus proof

--- scripts/cps2/run-corpus-smoke.ps1
+++ scripts/cps2/run-corpus-smoke.ps1
@@ -92,8 +92,8 @@ function Get-Cps2AudioFrameCount {
             $SetId -eq "armwar" -or $SetId -eq "avsp" -or $SetId -eq "batcir" -or
             $SetId -eq "choko" -or $SetId -eq "cybots" -or $SetId -eq "ddsom" -or
             $SetId -eq "ddtod" -or $SetId -eq "dimahoo" -or $SetId -eq "dstlk" -or
-            $SetId -eq "ecofghtr" -or $SetId -eq "gigawing" -or $SetId -eq "megaman2") -and
-        $DefaultFrames -lt 2500) {
+            $SetId -eq "ecofghtr" -or $SetId -eq "gigawing" -or $SetId -eq "megaman2" -or
+            $SetId -eq "mmatrix") -and $DefaultFrames -lt 2500) {
         return 2500
     }
 
@@ -127,8 +127,9 @@ function Test-Cps2AudioStateProbeDefault {
         $SetId -eq "choko" -or $SetId -eq "cybots" -or $SetId -eq "ddsom" -or
         $SetId -eq "ddtod" -or $SetId -eq "dimahoo" -or $SetId -eq "dstlk" -or
         $SetId -eq "ecofghtr" -or $SetId -eq "gigawing" -or $SetId -eq "hsf2" -or
-        $SetId -eq "megaman2" -or $SetId -eq "mshvsf" -or $SetId -eq "mvsc" -or
-        $SetId -eq "progear" -or $SetId -eq "ringdest" -or $SetId -eq "spf2t"
+        $SetId -eq "megaman2" -or $SetId -eq "mmatrix" -or $SetId -eq "mshvsf" -or
+        $SetId -eq "mvsc" -or $SetId -eq "progear" -or $SetId -eq "ringdest" -or
+        $SetId -eq "spf2t"
 }
 
 function Test-Cps2AudioGameplayProbeDefault {
@@ -139,7 +140,7 @@ function Test-Cps2AudioGameplayProbeDefault {
         $SetId -eq "choko" -or $SetId -eq "cybots" -or $SetId -eq "ddsom" -or
         $SetId -eq "ddtod" -or $SetId -eq "dimahoo" -or $SetId -eq "dstlk" -or
         $SetId -eq "ecofghtr" -or $SetId -eq "gigawing" -or $SetId -eq "ringdest" -or
-        $SetId -eq "megaman2" -or $SetId -eq "spf2t"
+        $SetId -eq "megaman2" -or $SetId -eq "mmatrix" -or $SetId -eq "spf2t"
 }
 
 function Get-Cps2AudioGameplayPlayerCount {
@@ -159,7 +160,7 @@ function Get-Cps2AudioGameplayPlayerCount {
     }
     if ($SetId -eq "cybots" -or $SetId -eq "dimahoo" -or $SetId -eq "dstlk" -or
         $SetId -eq "ecofghtr" -or $SetId -eq "gigawing" -or $SetId -eq "megaman2" -or
-        $SetId -eq "spf2t") {
+        $SetId -eq "mmatrix" -or $SetId -eq "spf2t") {
         return 2
     }
     return $DefaultPlayers
